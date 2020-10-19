39a01b36ca2bfd944602610ec1c5101f5e8badc9
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 09:23:54 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan1-Biodata/src/pboif2/pkg10119083/latihan1/biodata/PBOIF210119083Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama : Rafsan Zen Mustaofa
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Menamplkan Biodata 
+ */
+public class PBOIF210119083Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ATLIT" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t : Rafsan Zen Mustaofa ");
+        System.out.println("ALAMAT\t\t : Cianjur ");
+        System.out.println("TANGGAL LAHIR\t : 23 November 2000");
+        System.out.println("TEMPAT LAHIR\t : Cianjur");
+        System.out.println("------------------------");
+        System.out.println("HOBI\t\t         : Main Game ");
+        System.out.println("Makanan Kesukaan\t : INDOMIE ");
+        System.out.println("Minuman Kesukaan\t : Teh Pucuk ");
+    
+    }
+    
+}
